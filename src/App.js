79a39b0358f0d0c636e081b30e42b1c9603b9448
Rohith A <rohithar0411@gmail.com>
2025-12-12 10:38:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from "react";
+class App extends React.Component
+{
+  render()
+  {
+    return(
+      <>
+      <center><h1>Welcome to the Code World</h1>
+       <p>Web development is the process of creating websites and web applications that run on the internet. It involves two main parts: front-end development, which focuses on what users see and interact with on the screen using technologies like HTML, CSS, and JavaScript, and back-end development, which handles the server, database, and application logic using languages like Python, Java, PHP, or Node.js. Web development also includes ensuring that websites are responsive, meaning they work well on all devices, and interactive, providing smooth user experiences. Developers often use frameworks and libraries such as React, Angular, or Django to make development faster and more efficient. Overall, web development combines design, programming, and problem-solving to build functional and visually appealing digital experiences.</p>
+        </center>
+      </>
+    )
+  }
+}
+export default App
+
+
+
+
+// function App()
+// {
+//   return(
+//     <div>
+//     <h1>Welcome to the first day</h1>
+//     <h1>To Learn React</h1>
+//     <h1>Thank You</h1>
+//     </div>
+//   )
+// }
+// export default App
+
+
+
+
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
